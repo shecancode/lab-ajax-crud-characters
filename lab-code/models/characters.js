1c236fcb0bc58fc2c952078f368483f98d317991
Crystal Meneses <crystalm927@gmail.com>
2018-05-02 23:59:50 -0400

    still working on it

---
+++ lab-code/models/characters.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const autoIncrement = require("mongoose-auto-increment");
+
+
+  mongoose.connect('mongodb://localhost/lab-code', {useMongoClient: true});
+
+  autoIncrement.initialize(mongoose.connect('mongodb://localhost/lab-code', {useMongoClient: true}))
+
+
+  // const characterSchema = Schema ({
+    
+  //   id: type: Number, require: true, unique: true},
+  //   name: String,
+  //   occupation: String,
+  //   weapon: String,
+  //   cartoon: Boolean,
+
+  // });
+
+
+
+
+
+const characterSchema = Schema({
+  name:  String,
+  id: Number,
+  id: Number,
+  name: String,
+  occupation: String,
+  weapon: String,
+});
+
+
+
+  
+
+  // characterSchema.plugin(autoIncrement.plugin, {
+  //   model: 'characterSchema',
+  //   field: 'id',
+  //   startAt: 4,
+  //   incrementBy: 1
+  // });
+
+  const Character = mongoose.model("Character", characterSchema);
+
+  module.exports = Character;
